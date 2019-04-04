727c6236d24cd212997ecb34daae1e25d1febe01
Jaehyuk Yang <43870422+yangjae33@users.noreply.github.com>
2019-04-04 16:47:21 +0900

    190404

--- struct_family.cpp
+++ struct_family.cpp
@@ -28,67 +28,53 @@ private:
     int nodenum; // 이름 -> int로 저장했다가 인덱싱으로 이름 찾기
     string name;
 public:
-    vector<pair<man*,int>> addr; //연결되어있는 노드의 주소 및 관계 값
-    man(string name,int sex);
-    void insertMan(pair<man*,int> addr_and_rel){
-        addr.push_back(addr_and_rel);
+    vector<pair<int,int>> relation; //연결되어있는 노드의 주소 및 관계 값
+    man(string name,int _gender);
+    void insertMan(pair<int,int> addr_and_rel){
+        relation.push_back(addr_and_rel);
     };
     int getGender(){return gender;};
     int getNodeno(){return nodenum;};
-    man* getmyAddr(){return this;};
     string getName(){return name;};
-    pair<man*,int> getLastMember();
-};
-pair<man*,int> man::getLastMember(){
-    //vector<pair<man*,int>>::iterator iter = addr.end();
-    //pair<man*,int>result = make_tuple(iter->first,iter->second);
+    pair<int,int> getLastMember();
+    void updateName(string _name){name = _name;};
     
-    pair<man*,int> result = make_tuple(addr.back().first,addr.back().second);
+};
+pair<int,int> man::getLastMember(){
+
+    pair<int,int> result = make_tuple(relation.back().first,relation.back().second);
     return result;
 }
-man::man(string _name,int sex){
+man::man(string _name,int _gender){
     nodenum = ++_index;
-    gender = sex;
+    gender = _gender;
     name = _name;
 }
 class Graph{
 private:
     vector<man> family;
 public:
+    Connection con;
     Graph();    //사용자(자신) 추가
     void addMember();
     void updateMember();
     void findMember();
     void sql_Member();
     int find(int nodenum, queue<int>que, int* visited);
+    void show_Member();
 };
-Graph::Graph(){
-    Connection con(true);
-    try{
-        con.connect(DB_NAME,SERVER,USER,PWD);
-        cout<< "Connected"<<endl;
-    }catch(Exception &e){
-        cout<< e.what()<<endl;
-    }
-    
-    cout<<"input your name : ";
-    string _name;
-    cin>>_name;
-    int g;
-    cout<<"input your gender(0:male, 1:female) : ";
-    cin>>g;
-    man me(_name,g);
-    family.push_back(me);
-}
 void Graph::addMember(){
+    show_Member();
+    
     cout<<"input Standard of new family member : ";
-    int n;
-    cin>>n;
+    int n; cin>>n;
+    int rel;
+    int k = family[n-1].getNodeno();
     
     for(vector<man>::iterator iter = family.begin(); iter != family.end(); iter++){
         int temp = iter->getNodeno();
         
-        if(temp == n){
+        if(temp == k){
             cout<<"input name : ";
             string _name;cin>>_name;         //to enroll name
             cout<<"input relation(1:dad,2:mom,3:son,4:daughter,5:spouse) : ";
@@ -99,52 +85,132 @@ void Graph::addMember(){
                 gender = !(iter->getGender());
             man member = man(_name,gender);       //관계가 5면 1. 현재 성별과비교, 현재 성별이 0이면 1, 1이면 0
             
-            pair<man*,int> _addr_rel = make_tuple(&member,relation);
+            int target_index = _index;
+            pair<int,int> _addr_rel = make_tuple(target_index,relation);
+            iter->insertMan(_addr_rel);
             
-            (iter->getmyAddr())->insertMan(_addr_rel);
-            
-            if(relation <=2 && relation >=1)
-                _addr_rel = make_tuple(iter->getmyAddr(),3+iter->getGender());
-            else if(relation<=4 && relation >=1)
-                _addr_rel = make_tuple(iter->getmyAddr(),1+iter->getGender());
-            else if(relation == 5)
-                _addr_rel = make_tuple(iter->getmyAddr(),5);
-            else{
+            if(relation <=2 && relation >=1){
+                rel = 3+iter->getGender();
+                _addr_rel = make_tuple(iter->getNodeno(),3+iter->getGender());
+            }else if(relation<=4 && relation >=1){
+                rel = 3+iter->getGender();
+                _addr_rel = make_tuple(iter->getNodeno(),1+iter->getGender());
+            }else if(relation == 5){
+                rel = 3+iter->getGender();
+                _addr_rel = make_tuple(iter->getNodeno(),5);
+            }else{
                 cout<<"Error : input 1~5"<<endl;
                 continue;
             }
+            
             member.insertMan(_addr_rel);
-            /*
-            cout<<"기준이 되는 구성원: "<<(iter->getmyAddr())->getNodeno()<<endl;
+            
+            cout<<"기준이 되는 구성원: "<<iter->getNodeno()<<endl;
             cout<<"추가 구성원의 노드 번호 : "<<member.getNodeno()<<endl;
-            cout<<"기준 구성원 -> 추가 구성원의 관계 : "<<get<1>((iter->getmyAddr())->getLastMember())<<endl;
-            cout<<"추가 구성원 -> 기준 구성원의 관계 : "<<get<1>(get<0>((iter->getmyAddr())->getLastMember())->getLastMember())<<endl;
-            */
+            cout<<"기준 구성원 -> 추가 구성원의 관계 : "<<get<1>(iter->getLastMember())<<endl;
+            cout<<"추가 구성원 -> 기준 구성원의 관계 : "<<get<1>(member.getLastMember())<<endl;
+            
             family.push_back(member);
+            
+            //====================================================================
+            // 구성원 추가
+            //====================================================================
+            Query query = con.query();
+            query << "insert into member (name,gender) values ('"<<_name<<"'"<<","<<gender<<")";
+            query.store();
+            
+            //====================================================================
+            // 관계 추가
+            //====================================================================
+            query << "insert into relation (origin_idx,new_idx,origin_rel,new_rel) values ("<<member.getNodeno()<<","<<iter->getNodeno()<<","<<relation<<","<<rel<<")";
+            query.store();
+            
+            query << "insert into relation (origin_idx,new_idx,origin_rel,new_rel) values ("<<iter->getNodeno()<<","<<member.getNodeno()<<","<<rel<<","<<relation<<")";
+            query.store();
+            
             return;
         }
     }
     cout<<"Error : doesn't exist member that"<<endl;
 }
-void Graph::sql_Member(){
-    Connection con(true);
+Graph::Graph(){
+
     try{
         con.connect(DB_NAME,SERVER,USER,PWD);
         cout<< "Connected"<<endl;
     }catch(Exception &e){
         cout<< e.what()<<endl;
     }
+    
+    //======수정할부분
+    Query query = con.query();
+    query <<"select * ordered by idx from member";
+    StoreQueryResult init_index = query.store();
+    _index = (int)init_index.num_rows();
+    
+    query <<"select count(*) from member";
+    StoreQueryResult res = query.store();
+    if(res){
+        
+    }else{
+        cout<<"input your name : ";
+        string _name;
+        cin>>_name;
+        int g;
+        cout<<"input your gender(0:male, 1:female) : ";
+        cin>>g;
+        man me(_name,g);
+        family.push_back(me);
+    }
+}
+void Graph::show_Member(){
+    vector<man>::iterator iter;
+    int num = 1;
+    cout<<"========= Family Member ========="<<endl;
+    for(iter = family.begin(); iter!=family.end(); iter++){
+        cout<<num++<<". "<<iter->getName()<<endl;
+    }
+}
+
+void Graph::sql_Member(){
+    
     Query query = con.query();
     query <<"select * from member";
     StoreQueryResult res = query.store();
-    for(size_t i = 0;i<res.num_rows();i++)
+    
+    for(size_t i = 0;i<res.num_rows();i++){
+        man member = man(string(res[i]["name"]),res[i]["gender"]);
         cout<<"num : "<<res[i]["idx"] <<" name : "<<res[i]["name"]<<endl;
+        Query query1 = con.query();
+        query1 << "select * from relation where origin_idx = "<<res[i]["idx"];
+        StoreQueryResult res_1 = query1.store();
+        
+        for (size_t j =0 ;j < res_1.num_rows();j++){
+            pair<int,int> temp_pair;
+            temp_pair = make_tuple(int(res_1[j]["origin_rel"]),int(res_1[j]["new_rel"]));
+            member.relation.push_back(temp_pair);
+        }
+        family.push_back(member);
+    }
 }
 void Graph::updateMember(){
+    show_Member();
+    cout<<"input member num to update name : ";
+    int n;
+    cin>>n;
+    cout<<"input new name : ";
+    string new_name;
+    cin>>new_name;
+    
+    family[n-1].updateName(new_name);
+    n = family[n-1].getNodeno();
+    
+    Query query1 = con.query();
+    query1 << "update member set name='"<<new_name<<"' where idx="<<n;
+    query1.store();
+    
+    cout<<"== update completed =="<<endl;
 }
-//============================
-
-//=============================
 
 int Graph::find(int nodenum,queue<int>que,int* visited){
     //nodenum : 찾아야할 노드번호
@@ -164,12 +230,12 @@ int Graph::find(int nodenum,queue<int>que,int* visited){
         else
             visited[cur_node-1] = 1;    //방문 표시
         
-        if(family[cur_node-1].addr.size()==0)
+        if(family[cur_node-1].relation.size()==0)
             return 0;
         else{
             _count++;
-            for(int i = 0; i<family[cur_node-1].addr.size(); i++){
-                que.push(get<1>(family[cur_node-1].addr[i]));
+            for(int i = 0; i<family[cur_node-1].relation.size(); i++){
+                que.push(get<1>(family[cur_node-1].relation[i]));
             }
         }
     }
@@ -199,33 +265,20 @@ void Graph::findMember(){
             visited[me-1] = 1;
             que.push(me);
             cout<<find(nodenum, que, visited)<<"촌 관계입니다."<<endl;
-            
         }
     }
 }
-//================================
 
 int main(int argc, const char * argv[]){
     
     Graph* Grp = new Graph;
-    
-    //=================
-    //=====DB TEST=====
-    
 
-    /*
-    Query query = con.query();
-    query <<"insert into test values(0,'jaehyuk');";
-    query.execute();
-    
-    query <<"select * from test;";
-    StoreQueryResult res = query.store();
-    for(size_t i = 0;i<res.num_rows();i++)
-        cout<<"num : "<<res[i]["num"] <<" name : "<<res[i]["name"]<<endl;
-    */
     int select;
+    Grp->sql_Member();
+    
+    Grp->show_Member();
     while(true){
-        cout<<"=== MENU ===="<<endl;
+        cout<<"========= MENU ========="<<endl;
         cout<<"1. add Member"<<endl;
         cout<<"2. update Member"<<endl;
         cout<<"3. find Member"<<endl;
